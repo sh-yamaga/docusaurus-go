fbd3b819c25758a99a767fbc666759152dbd20aa
yamag-shu <s.yamaga.0318@gmail.com>
2024-05-27 16:01:16 +0900

    BrokenLinksの設定変更

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
 	organizationName: "sh-yamaga", // Usually your GitHub org/user name.
 	projectName: "docusaurus-go", // Usually your repo name.
 
-	onBrokenLinks: "throw",
+	onBrokenLinks: "warn",
 	onBrokenMarkdownLinks: "warn",
 
 	// Even if you don't use internationalization, you can use this field to set
